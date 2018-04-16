8cf4378907a8f6a9b28db9bd0596e5dfa835ccb9
Ojas Kale <ojas.kale@tdstelecom.com>
2018-04-16 13:09:44 -0500

    Adds image to specified folder

--- main/models.py
+++ main/models.py
@@ -4,10 +4,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+from datetime import datetime
+
+
+def user_directory_path(instance, filename):
+    # import pdb; pdb.set_trace()
+    return 'uploads/{0}/{1}'.format( instance.project.name,
+        datetime.now().strftime('%Y%m%d%H%M%S')+'_'+filename
+    )
+
+
 class Project(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
 class ProjectImage(models.Model):
    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-   image = models.ImageField()
+   image = models.ImageField(upload_to=user_directory_path)
